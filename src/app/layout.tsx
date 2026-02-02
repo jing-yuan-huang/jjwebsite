285e302b071d26539d49398a8a69b4fd3d21066c
jing-yuan-huang <r11944029@csie.ntu.edu.tw>
2026-02-02 15:51:07 +0800

    chore: enable Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Archivo } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react";
 
 const archivo = Archivo({
   subsets: ['latin'],
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className="min-h-screen">
         <Header />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
